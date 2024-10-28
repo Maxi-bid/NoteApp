69ba8927b0b7e532eae7e1abe857249695cf47c2
CarlosDur4n <carloncho3123@gmail.com>
2024-10-28 10:25:12 -0300

    nuevo comit

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -2,10 +2,12 @@ package com.example.noteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     TextView entrevista;
+    EditText nota1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
