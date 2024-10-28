c737c6038090b325ae8ee23156282b2a0abecb1e
maxi_bid <ardor2411@gmail.com>
2024-10-28 10:46:14 -0300

    ingreso al proyecto

--- app/src/main/java/com/example/noteapp/MainActivity.java
+++ app/src/main/java/com/example/noteapp/MainActivity.java
@@ -2,15 +2,31 @@ package com.example.noteapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-    TextView entrevista;
+
     EditText nota1;
+    Button ingresa, eliminar;
+    RecyclerView RecyclerVV;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        nota1 = findViewById(R.id.TxtNota);
+        ingresa = findViewById(R.id.btnIngresar);
+        eliminar = findViewById(R.id.btnEliminar);
+        RecyclerVV = findViewById(R.id.RecyclerV);
+        ingresa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
     }
 }
